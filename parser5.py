a2122e126049d8c544ec1da91df2b16b7b20f0ec
Ka7iposha <mixail.sakulin@bk.ru>
2023-06-22 11:11:33 +0300

    Update parser5.py

--- parser5.py
+++ parser5.py
@@ -2,6 +2,7 @@ import requests
 import logging
 import os
 import csv
+import pandas as pd
 
 '''
 
@@ -94,35 +95,33 @@ def get_data_from_json(json_file):
     return data_list
 
 
-def get_content(output_file, shard, query, low_price=None, top_price=None):
+def get_content(shard, query, low_price=None, top_price=None):
     # вставляем ценовые рамки для уменьшения выдачи, вайлдберис отдает только 100 страниц
     headers = {'Accept': "*/*", 'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"}
     data_list = []
-    with open(f'{output_file}', "w", encoding='utf16', newline='') as output_file:
-        writer = csv.writer(output_file, delimiter=';')
-        writer.writerow(['title', 'price', 'currency', 'url'])
-        for page in range(1, 101):
-            logger.info(f'Сбор позиций со страницы {page} из 100')
-            url = f'https://catalog.wb.ru/catalog/{shard}/catalog?appType=1&curr=rub&dest=-1075831,-77677,-398551,12358499' \
-                  f'&locale=ru&page={page}&priceU={low_price * 100};{top_price * 100}' \
-                  f'&reg=0&regions=64,83,4,38,80,33,70,82,86,30,69,1,48,22,66,31,40&sort=popular&spp=0&{query}'
-            r = requests.get(url, headers=headers)
-            data = r.json()
-            logger.info(f'Добавлено позиций: {len(get_data_from_json(data))}')
-            if len(get_data_from_json(data)) > 0:
-                data_list.extend(get_data_from_json(data))
-            else:
-                logger.info(f'Сбор данных завершен.')
-                break
-            writer.writerow(data_list)
+    for page in range(1, 101):
+        logger.info(f'Сбор позиций со страницы {page} из 100')
+        url = f'https://catalog.wb.ru/catalog/{shard}/catalog?appType=1&curr=rub&dest=-1075831,-77677,-398551,12358499' \
+              f'&locale=ru&page={page}&priceU={low_price * 100};{top_price * 100}' \
+              f'&reg=0&regions=64,83,4,38,80,33,70,82,86,30,69,1,48,22,66,31,40&sort=popular&spp=0&{query}'
+        r = requests.get(url, headers=headers)
+        data = r.json()
+        logger.info(f'Добавлено позиций: {len(get_data_from_json(data))}')
+        if len(get_data_from_json(data)) > 0:
+            data_list.extend(get_data_from_json(data))
+        else:
+            logger.info(f'Сбор данных завершен.')
+            break
     return data_list
 
 
-#def save_excel(data):
-  #  """сохранение результата в excel файл"""
- #   with open(r"C:\Users\mastermor\Desktop\parser.txt", 'a', encoding='UTF-8') as file:
-   #     file.write(data)
-
+def save_excel(data, filename):
+    """сохранение результата в excel файл"""
+    df = pd.DataFrame(data)
+    writer = pd.ExcelWriter(f'{filename}.xlsx')
+    df.to_excel(writer, 'data')
+    writer.close()
+    print(f'Все сохранено в {filename}.xlsx')
 
 
 def parser(url, low_price, top_price):
@@ -132,14 +131,13 @@ def parser(url, low_price, top_price):
         # поиск введенной категории в общем каталоге
         name_category, shard, query = search_category_in_catalog(url=url, catalog_list=catalog_list)
         # сбор данных в найденном каталоге
-        xlsx_path = os.path.dirname(__file__) + r'\parserr.csv'
-        data_list = get_content(xlsx_path, shard=shard, query=query, low_price=low_price, top_price=top_price)
+        data_list = get_content(shard=shard, query=query, low_price=low_price, top_price=top_price)
         # сохранение найденных данных
-        # save_excel(data_list)
+        save_excel(data_list, f'{name_category}_from_{low_price}_to_{top_price}')
     except TypeError:
-        logger.info('Ошибка! Возможно не верно указан раздел. Удалите все доп фильтры с ссылки')
+        print('Ошибка! Возможно не верно указан раздел. Удалите все доп фильтры с ссылки')
     except PermissionError:
-        logger.info('Ошибка! Вы забыли закрыть созданный ранее excel файл. Закройте и повторите попытку')
+        print('Ошибка! Вы забыли закрыть созданный ранее excel файл. Закройте и повторите попытку')
 
 
 if __name__ == '__main__':
